055483e2ad2797eca8c8bfb83affa847159d397f
ix-t <107044773+ix-t@users.noreply.github.com>
2022-10-01 20:55:36 -0500

    Background images change updated
    - Images added, renamed and relocated
    - WeatherImage.js updated

--- src/WeatherImage.js
+++ src/WeatherImage.js
@@ -1,37 +1,38 @@
 import React from "react";
-//import Background from `./img/03n-image.jpg`;
 
 export default function WeatherImage(props) {
   const codeMapping = {
-    "01d": "01d-01n-clear-sky.jpg",
-    "02d": "02d-02n-few-clouds.jpg",
-    "03d": "03d-03n-scattered-clouds.jpg",
-    "04d": "04d-04n-broken-clouds.jpg",
-    "09d": "09d-09n-shower-rain.jpg",
-    "10d": "10d-10n-rain.jpg",
-    "11d": "11d-11n-thunderstorm.jpg",
-    "13d": "13d-13n-snow.jpg",
-    "50d": "50d-50n-mist.jpg",
-    "01n": "01d-01n-clear-sky.jpg",
-    "02n": "02d-02n-few-clouds.jpg",
-    "03n": "03d-03n-scattered-clouds.jpg",
-    "04n": "04d-04n-broken-clouds.jpg",
-    "09n": "09d-09n-shower-rain.jpg",
-    "10n": "10d-10n-rain.jpg",
-    "11n": "11d-11n-thunderstorm.jpg",
-    "13n": "13d-13n-snow.jpg",
-    "50n": "50d-50n-mist.jpg",
+    "01d": "01d-image.jpg",
+    "02d": "02d-image.jpg",
+    "03d": "03d-image.jpg",
+    "04d": "04d-image.jpg",
+    "09d": "09d-image.jpg",
+    "10d": "10d-image.jpg",
+    "11d": "11d-image.jpg",
+    "13d": "13d-image.jpg",
+    "50d": "50d-image.jpg",
+    "01n": "01n-image.jpg",
+    "02n": "02n-image.jpg",
+    "03n": "03n-image.jpg",
+    "04n": "04n-image.jpg",
+    "09n": "09n-image.jpg",
+    "10n": "10n-image.jpg",
+    "11n": "11n-image.jpg",
+    "13n": "13n-image.jpg",
+    "50n": "50n-image.jpg",
   };
 
-  console.log(props.data.iconCode);
-  let iconCode = codeMapping[props.data.iconCode];
+  console.log(codeMapping[props.data.iconCode]);
+  let iconImage = codeMapping[props.data.iconCode];
 
   return (
     <div
       style={{
-        backgroundImage: `url(./img/${iconCode}-image.jpg)`,
+        backgroundImage: `url(/img/${iconImage})`,
       }}
       className="background-image"
-    ></div>
+    >
+      Image
+    </div>
   );
 }
